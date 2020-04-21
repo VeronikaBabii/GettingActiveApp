7b01ce14c71abbd1fb123e24300d5c0d276c1bcd
VeronikaBabii <veronika.babii.2019@gmail.com>
2020-04-22 00:18:33 +0300

    First commit - authentication done

---
+++ GettingActiveApp/Helpers/Constants.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+
+struct Constants {
+    
+    struct Storyboard {
+        
+        // shortcut to reference HomeVC storyboard 
+        // static - so we can access it without having to create instances of the structure
+        static let homeViewController = "HomeVC"
+        
+    }
+    
+}
---
+++ GettingActiveApp/View Controllers/SignUpViewController.swift
@@ -0,0 +1,129 @@
+//
+//  SignUpViewController.swift
+//  GettingActiveApp
+//
+//  Created by Veronika Babii on 19.04.2020.
+//  Copyright © 2020 Veronika Babii. All rights reserved.
+//
+
+import UIKit
+import Firebase
+import FirebaseAuth
+import FirebaseFirestore
+
+class SignUpViewController: UIViewController {
+
+    @IBOutlet weak var firstNameTextField: UITextField!
+    
+    @IBOutlet weak var lastNameTextField: UITextField!
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var signUpButton: UIButton!
+    
+    @IBOutlet weak var errorLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        setUpElements()
+    }
+    
+    func setUpElements() {
+        
+        // hide the error label
+        errorLabel.alpha = 0
+        
+        // style the elements
+        Utilities.styleTextField(firstNameTextField)
+        Utilities.styleTextField(lastNameTextField)
+        Utilities.styleTextField(emailTextField)
+        Utilities.styleTextField(passwordTextField)
+        Utilities.styleFilledButton(signUpButton)
+        
+    }
+    
+    // check the fields and validate that the data is correct.
+    // if everything is correct - return nil (optional type). otherwise, return error message. (method return optional String)
+    func validateFields() -> String? {
+        
+        // check that all fields are filled in
+        // if when we get rid of all white spaces and new lines in a field and still have "" (any of fields are empty), then return error
+        if firstNameTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" ||
+            lastNameTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" ||
+            emailTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" ||
+            passwordTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" {
+            return "Please fill in all fields!"
+        }
+        
+        // check if the password is secure ( ! is unwrapper from ?, means that there is a text for sure)
+        let cleanedPassword = passwordTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        // password isn't secure enough
+        if Utilities.isPasswordValid(cleanedPassword) == false {
+            return "Please make sure your password is at least 8 characters, contains a special character and a number!"
+        }
+        
+        return nil
+    }
+
+    @IBAction func signUpTapped(_ sender: Any) {
+        
+        // validate the fields
+        let error = validateFields()
+        
+        // there's an error, bc something wrong with the fields, so show error message
+        if error != nil {
+            showError(error!)
+        } else { // no errors
+            
+            // create cleaned versions of the data
+            let firstname = firstNameTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+            let lastname = lastNameTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+            let email = emailTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+            let password = passwordTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+            
+            // create the user; return err in case of error; return uid via result when successful
+            Auth.auth().createUser(withEmail: email, password: password) { (result, err) in // code that gets run when the user is created
+                
+                // there was an error creating the user
+                if err != nil  {
+                    self.showError("Error creating user!")
+                } else { // no errors, user was created successfully, so store the firstname and the lastname
+                    let db = Firestore.firestore()
+                    
+                    // push user data to the db
+                    db.collection("users").addDocument(data: ["firstname":firstname, "lastname":lastname, "uid":result!.user.uid]) { (error) in
+                        // handle error
+                         if error != nil  {
+                            self.showError("Error saving user data!")
+                        }
+                    }
+                    // transition to the home screen
+                    self.transitionToHome()
+                }
+            }
+        }
+    }
+    
+    // method to show error message on the page
+    func showError(_ message:String) {
+        errorLabel.text = message // show error message in our label
+        errorLabel.alpha = 1 // visible
+    }
+    
+    // method to go to the home screen (sign rootVC to any other VC we're now)
+    func transitionToHome() {
+        // reference to HomeViewController (this returns view controller, so we cast it as (?as) HomeViewController type)
+        let homeViewController = storyboard?.instantiateViewController(identifier: Constants.Storyboard.homeViewController) as? HomeViewController
+        
+        // swap - sign homeVC to the rootVC
+        view.window?.rootViewController = homeViewController
+        // show homeVC as rootVC instead
+        view.window?.makeKeyAndVisible()
+    }
+    
+}
